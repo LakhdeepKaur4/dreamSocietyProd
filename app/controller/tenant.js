b870347dd42e57cb71fdfb4fc71718050bfb0943
LakhdeepKaur4 <lakhdeep.kaur@greatwits.com>
2019-04-17 14:35:33 +0530

    tenant

--- app/controller/tenant.js
+++ app/controller/tenant.js
@@ -108,39 +108,39 @@ function decrypt1(key, data) {
     return decrypted;
 }
 
-let mailToUser = (email, tenantId) => {
-    const token = jwt.sign(
-        { data: 'foo' },
-        'secret', { expiresIn: '1h' });
-    tenantId = encrypt(tenantId.toString());
-    const request = mailjet.post("send", { 'version': 'v3.1' })
-        .request({
-            "Messages": [
-                {
-                    "From": {
-                        "Email": "rohit.khandelwal@greatwits.com",
-                        "Name": "Greatwits"
-                    },
-                    "To": [
-                        {
-                            "Email": email,
-                            "Name": 'Atin' + ' ' + 'Tanwar'
-                        }
-                    ],
-                    "Subject": "Activation link",
-                    "HTMLPart": `<b>Click on the given link to activate your account</b> <a href="http://mydreamsociety.com/login/tokenVerification?tenantId=${tenantId}&token=${token}">click here</a>`
-                }
-            ]
-        })
-    request
-        .then((result) => {
-            console.log(result.body)
-            // console.log(`http://192.168.1.105:3000/submitotp?userId=${encryptedId}token=${encryptedToken}`);
-        })
-        .catch((err) => {
-            console.log(err.statusCode)
-        })
-}
+// let mailToUser = (email, tenantId) => {
+//     const token = jwt.sign(
+//         { data: 'foo' },
+//         'secret', { expiresIn: '1h' });
+//     tenantId = encrypt(tenantId.toString());
+//     const request = mailjet.post("send", { 'version': 'v3.1' })
+//         .request({
+//             "Messages": [
+//                 {
+//                     "From": {
+//                         "Email": "rohit.khandelwal@greatwits.com",
+//                         "Name": "Greatwits"
+//                     },
+//                     "To": [
+//                         {
+//                             "Email": email,
+//                             "Name": 'Atin' + ' ' + 'Tanwar'
+//                         }
+//                     ],
+//                     "Subject": "Activation link",
+//                     "HTMLPart": `<b>Click on the given link to activate your account</b> <a href="http://mydreamsociety.com/login/tokenVerification?tenantId=${tenantId}&token=${token}">click here</a>`
+//                 }
+//             ]
+//         })
+//     request
+//         .then((result) => {
+//             console.log(result.body)
+//             // console.log(`http://192.168.1.105:3000/submitotp?userId=${encryptedId}token=${encryptedToken}`);
+//         })
+//         .catch((err) => {
+//             console.log(err.statusCode)
+//         })
+// }
 
 let mailToOwner = async (ownerId, tenant) => {
     // let email = decrypt1(key,owner.email);
