d366d6d57470c726331f1184ae57f8f215ff830a
rohit231095 <rohit.khandelwal@greatwits.com>
2019-05-31 19:10:13 +0530

    OWNER2000

--- app/controller/owner.js
+++ app/controller/owner.js
@@ -556,7 +556,7 @@ exports.get1 = async (req, res, next) => {
     // console.log(owners);
 
     owners.map(owner => {
-      console.log("adsfag===", ownerId);
+      // console.log("adsfag===", ownerId);
       owner.firstName = decrypt(key, owner.firstName);
       owner.lastName = decrypt(key, owner.lastName);
       owner.userName = decrypt(key, owner.userName);
